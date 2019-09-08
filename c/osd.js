f4a6044aaf10d4c836b80ec2453ee7dab173a902
Santiago Ferreira <san650@gmail.com>
2019-09-08 20:15:56 -0300

    Show level title

--- c/osd.js
+++ c/osd.js
@@ -1,7 +1,7 @@
 export default function(game, level) {
   return `
     <div class="osd">
-      <span class="level-title">Level ${game.level}</span>
+      <span class="title">Level ${game.level}</span>
       <span class="lives" id="lives">♡  ${game.lives}</span>
       <span class="words" id="words">${level.target - level.hits}</span>
       <span class="points" id="points">${game.points}</span>
