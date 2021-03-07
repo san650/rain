db938d8faad95e05ce6a123792f066970f1c5e3f
Santiago Ferreira <san650@gmail.com>
2021-03-07 15:55:01 -0300

    Rename folder to assets

--- cc/video.js
+++ cc/video.js
@@ -14,55 +14,55 @@ export function randomVideo() {
 
 const VIDEOS = [
   {
-    src: './bg/Words.mp4',
-    poster: './bg/Words.jpg'
+    src: './assets/Words.mp4',
+    poster: './assets/Words.jpg'
   },
   {
-    src: './bg/Stormy_Night.mp4',
-    poster: './bg/Stormy_Night.jpg'
+    src: './assets/Stormy_Night.mp4',
+    poster: './assets/Stormy_Night.jpg'
   },
   {
-    src: './bg/Cloud_Surf.mp4',
-    poster: './bg/Cloud_Surf.jpg'
+    src: './assets/Cloud_Surf.mp4',
+    poster: './assets/Cloud_Surf.jpg'
   },
   {
-    src: './bg/Bull-Creek.mp4',
-    poster: './bg/Bull-Creek.jpg'
+    src: './assets/Bull-Creek.mp4',
+    poster: './assets/Bull-Creek.jpg'
   },
   {
-    src: './bg/Twisted-Fire.mp4',
-    poster: './bg/Twisted-Fire.jpg'
+    src: './assets/Twisted-Fire.mp4',
+    poster: './assets/Twisted-Fire.jpg'
   },
   {
-    src: './bg/Escalate.mp4',
-    poster: './bg/Escalate.jpg'
+    src: './assets/Escalate.mp4',
+    poster: './assets/Escalate.jpg'
   },
   {
-    src: './bg/Drips.mp4',
-    poster: './bg/Drips.jpg'
+    src: './assets/Drips.mp4',
+    poster: './assets/Drips.jpg'
   },
   {
-    src: './bg/Busy.mp4',
-    poster: './bg/Busy.jpg'
+    src: './assets/Busy.mp4',
+    poster: './assets/Busy.jpg'
   },
   {
-    src: './bg/Beetle-Nut-Trees.mp4',
-    poster: './bg/Beetle-Nut-Trees.jpg'
+    src: './assets/Beetle-Nut-Trees.mp4',
+    poster: './assets/Beetle-Nut-Trees.jpg'
   },
   {
-    src: './bg/Beetle-Nut-Trees.mp4',
-    poster: './bg/Beetle-Nut-Trees.jpg'
+    src: './assets/Beetle-Nut-Trees.mp4',
+    poster: './assets/Beetle-Nut-Trees.jpg'
   },
   {
-    src: './bg/Night-Traffic.mp4',
-    poster: './bg/Night-Traffic.jpg'
+    src: './assets/Night-Traffic.mp4',
+    poster: './assets/Night-Traffic.jpg'
   },
   {
-    src: './bg/Pigeon-Impossible.mp4',
-    poster: './bg/Pigeon-Impossible.jpg'
+    src: './assets/Pigeon-Impossible.mp4',
+    poster: './assets/Pigeon-Impossible.jpg'
   },
   {
-    src: './bg/Textures.mp4',
-    poster: './bg/Textures.jpg'
+    src: './assets/Textures.mp4',
+    poster: './assets/Textures.jpg'
   },
 ];
